de66af0bc58550f8956089a2899f846dff55e480
Sheena Katrina <sheenakatrina@gmail.com>
2018-05-04 19:16:44 +0800

    initial set up

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          <Button color="primary">primary</Button>{' '}
+          <Button color="secondary">secondary</Button>{' '}
+          <Button color="success">success</Button>{' '}
+          <Button color="info">info</Button>{' '}
+          <Button color="warning">warning</Button>{' '}
+          <Button color="danger">danger</Button>{' '}
+          <Button color="link">link</Button>
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
